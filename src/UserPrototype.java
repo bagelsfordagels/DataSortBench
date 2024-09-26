6d844182e0382db00ac71a4412e2ab2ae430345f
bagelsfordagels <74632414+bagelsfordagels@users.noreply.github.com>
2024-09-26 14:21:40 -0400

    fixed userPrototype

--- src/UserPrototype.java
+++ src/UserPrototype.java
@@ -2,28 +2,19 @@ public class UserPrototype{
 	public void prototype(DataStorageSystem dss) {
 		//send data to data storage system
 		//test to compile
-<<<<<<< HEAD
-		//key is associated with specific data storage system need
-		int key = 1;
-		dss.sendData(key);
-		
-		// retrieve data from storage system
-		
-		char[] sortedData = dss.recieveData(key);
-=======
 		String[] arr = {"apple","bannana"};
 		//key is associated with specific data storage system needs
-		int key = dss.sendData(arr);
+		char[] c = dss.sendData(1);
 
 		// retrieve data from storage system
 
-		dss.recieveData(key);
+		dss.recieveData(1);
 		
 		InputConfig inputConfig = new InputConfig() {
 			
 		};
 		OutputConfig outputConfig = null;
 		ComputeRequest request = new ComputeRequest(inputConfig, outputConfig, ',');
->>>>>>> main
+
 	}
 }
