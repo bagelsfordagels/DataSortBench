e25a349998b18ed4caad59b6580289bac1aeff44
Suchya2 <suchya2@newpaltz.edu>
2024-09-26 15:07:00 -0400

    Update TestInputConfig.java

--- test/TestInputConfig.java
+++ test/TestInputConfig.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
 import java.util.List;
-public class testInputConfig implements InputConfig{
+public class TestInputConfig implements InputConfig{
 	private List<Integer> input = new ArrayList<>();
 }
