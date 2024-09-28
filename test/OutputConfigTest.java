fba6118f3c7fe2c0f76337bb936b488d4ddbc07c
Suchya2 <suchya2@newpaltz.edu>
2024-09-28 17:00:17 -0400

    possible fix

--- test/OutputConfigTest.java
+++ test/OutputConfigTest.java
@@ -4,9 +4,6 @@ import java.util.List;
 public class OutputConfigTest implements OutputConfig{
 	public List<char[]> outputData = new ArrayList<>();
 	public List<char[]> getOutputData(){
-		return outputData;
-	import java.util.ArrayList;
-	import java.util.List;
+			return outputData;
 		}
 	}
- }
