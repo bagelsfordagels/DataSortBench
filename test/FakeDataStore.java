0d87caa57870a9095db2439adec30d0afc8d47c0
Suchya2 <suchya2@newpaltz.edu>
2024-09-28 19:05:09 -0400

    possible checkstylefix

--- test/FakeDataStore.java
+++ test/FakeDataStore.java
@@ -8,32 +8,32 @@ public class FakeDataStore implements DataStorageSystem{
 		return null;
 	}
 	public boolean testDataStore() {
-		TestInputConfig InputConfig = new TestInputConfig();
-		OutputConfigTest OutputConfig = new OutputConfigTest();
+		TestInputConfig inputConfig = new TestInputConfig();
+		OutputConfigTest outputConfig = new OutputConfigTest();
 		int incount = 0;
 		int outcount = 0;
 		//tests if the input List are empty
-		if(InputConfig.getInputData().size() == 0) {
+		if(inputConfig.getInputData().size() == 0) {
 			System.out.println("Input list is empty");
 			return false;
 		}
 		//tests if the output List is empty
-		if(OutputConfig.getOutputData().size() == 0) {
+		if(outputConfig.getOutputData().size() == 0) {
 			System.out.println("Output List is Empty");
 			return false;
 		}
 		//runs through the input list to make sure it can be read
-		for(int i=0;i<InputConfig.getInputData().size();i++) {
+		for(int i=0;i<inputConfig.getInputData().size();i++) {
 			incount++;
 		}
-		if(InputConfig.getInputData().size() == incount) {
+		if(inputConfig.getInputData().size() == incount) {
 			return true;
 		}
 		//runs through the output list to make sure that it can be read
-		for(int i=0;i<OutputConfig.getOutputData().size();i++) {
+		for(int i=0;i<outputConfig.getOutputData().size();i++) {
 			outcount++;
 		}
-		if(OutputConfig.getOutputData().size() == outcount) {
+		if(outputConfig.getOutputData().size() == outcount) {
 			return true;
 		}
 		return false;
