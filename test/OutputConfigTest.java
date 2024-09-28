f1ec7dad76d717629845da7ccd5b6c75c8406b44
Suchya2 <suchya2@newpaltz.edu>
2024-09-28 16:05:20 -0400

    FakeDataStore

---
+++ test/OutputConfigTest.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class OutputConfigTest implements OutputConfig{
+	public List<char[]> outputData = new ArrayList<>();
+	public List<char[]> getOutputData(){
+		return outputData;
+	import java.util.ArrayList;
+	import java.util.List;
+		}
+	}
+ }
