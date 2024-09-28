fe034091da16cb7f5ebea906142ec5253ae41c92
Suchya2 <suchya2@newpaltz.edu>
2024-09-28 17:02:06 -0400

    possible fix

--- test/OutputConfigTest.java
+++ test/OutputConfigTest.java
@@ -1,9 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
-
 public class OutputConfigTest implements OutputConfig{
 	public List<char[]> outputData = new ArrayList<>();
 	public List<char[]> getOutputData(){
 			return outputData;
-		}
 	}
+}
--- test/TestInputConfig.java
+++ test/TestInputConfig.java
@@ -1,6 +1,5 @@
 import java.util.ArrayList;
 import java.util.List;
-
 public class TestInputConfig implements InputConfig{
 	public List<Integer> inputData = new ArrayList<>();
 	public List<Integer> getInputData(){
