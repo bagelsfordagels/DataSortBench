b20d60d1e00a77cdad121b583206d926732f7f66
Suchya2 <suchya2@newpaltz.edu>
2024-09-30 15:54:36 -0400

    possible fix

--- src/IntegerInputConfig.java
+++ src/IntegerInputConfig.java
@@ -7,5 +7,11 @@ public class IntegerInputConfig implements InputConfig{
 		// TODO Auto-generated method stub
 		return 0;
 	}
+
+	@Override
+	public String getUserFileData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
--- src/StoragePrototype.java
+++ src/StoragePrototype.java
@@ -1,8 +1,8 @@
 public class StoragePrototype{
 	public void prototype(StorageToLoad stl) {
-		char[] arr = {'a','b','c'};
+		
 		//stores the data and gives it a key to be accessed later
-		stl.storedata(arr);
+		stl.storeInputdata();
 		//takes the key given to find and give back the sorted array
 		stl.returndata(arr);
 	}
