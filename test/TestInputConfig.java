142e23ab1b183657d608b4137a2d1b9a54729b12
bagelsfordagels <74632414+bagelsfordagels@users.noreply.github.com>
2024-09-30 15:29:16 -0400

    gradle fix

--- test/TestInputConfig.java
+++ test/TestInputConfig.java
@@ -2,4 +2,10 @@ import java.util.ArrayList;
 import java.util.List;
 public class TestInputConfig implements InputConfig{
 	private List<Integer> input = new ArrayList<>();
+
+	@Override
+	public int getUserData() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
