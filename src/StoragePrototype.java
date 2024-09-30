cd6144858074219c0a4663b111f69bddcb4ff994
Suchya2 <suchya2@newpaltz.edu>
2024-09-30 15:59:19 -0400

    possible fix

--- src/StoragePrototype.java
+++ src/StoragePrototype.java
@@ -1,8 +1,8 @@
 public class StoragePrototype{
 	public void prototype(StorageToLoad stl) {
-		
+		char[] arr = {'a','b','c'};
 		//stores the data and gives it a key to be accessed later
-		stl.storeInputdata();
+		stl.storedata(arr);
 		//takes the key given to find and give back the sorted array
 		stl.returndata(arr);
 	}
