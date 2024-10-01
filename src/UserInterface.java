f9b36e17485b2425951582a417ca181ecaaa8e1a
bagelsfordagels <74632414+bagelsfordagels@users.noreply.github.com>
2024-10-01 13:46:07 -0400

    Update UserInterface.java

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -10,14 +10,16 @@ public class UserInterface{
 		System.out.println("If you would like to enter an integer enter 1/nif you would like to enter a file enter 2:");
 		
 		int userChoice = 0;
-		try {
-			userChoice = userData.nextInt();
-		} catch(InputMismatchException e) {
-			System.out.println("Invalid input. Please enter 1 or a file name.");
-			userData.nextLine(); // Clear the invalid input from the scanner buffer
-			userData.close();
-			return; // Exit the program if input is not an integer
-		}
+		do {
+			try {
+				userChoice = userData.nextInt();
+				break;
+			} catch(InputMismatchException e) {
+				System.out.println("Invalid input. Please enter 1 or a file name.");
+				userData.nextLine(); // Clear the invalid input from the scanner buffer
+			}
+		}while(true);
+		
 		
 		ComputeEngineStorageSystem css = new ComputeEngineStorageImplementation();
 		if(userChoice == 1) {
@@ -36,7 +38,7 @@ public class UserInterface{
 				System.out.print(sortedArr[i] + " ");
 			}
 		}
-		else if(userChoice == 2) {
+		if(userChoice == 2) {
 			System.out.println("Enter the file name: ");
 			String userInput = userData.next();
 			
