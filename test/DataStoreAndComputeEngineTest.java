59c313a86c3bfdd7ea37f2c4bae9f055d7ddc134
Suchya2 <suchya2@newpaltz.edu>
2024-10-03 22:58:10 -0400

    Update DataStoreAndComputeEngineTest.java

--- test/DataStoreAndComputeEngineTest.java
+++ test/DataStoreAndComputeEngineTest.java
@@ -5,15 +5,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class DataStoreAndComputeEngineTest{
+	//checks to make sure that the amount of char[] is the same as the amount of integers in the file
 	@Test
-	public void dSandCESmokeTest(){
+	public void arrayConversionTest(){
 		DataStorageSystem testDSImplementation = new DataStorageImplementation();
 		ComputeEngine testComputeEngine = new ComputeEngine();
 		InputConfig testFile = new FileInputConfig("TestFile");
 		UUID key = testDSImplementation.sendData(testFile);
 		ArrayList<Integer> testAl = testDSImplementation.recieveData(key);
 		ArrayList<char[]> sortedFileArr = testComputeEngine.readFile(testAl);
-		//checks to make sure that the amount of char[] is the same as the amount of integers in the file
 		Assertions.assertEquals(sortedFileArr.size(),testAl.size());
 	}
 }
