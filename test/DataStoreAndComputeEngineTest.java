4fd4dfd0c45a343e45749d76ab80517fa2038277
Suchya2 <suchya2@newpaltz.edu>
2024-10-03 22:53:58 -0400

    DataStoreAndComputeEngineTest
    Added a test to make sure the amount of integers in a file is the same amount of char[]'s we give back

---
+++ test/DataStoreAndComputeEngineTest.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DataStoreAndComputeEngineTest{
+	@Test
+	public void DSandCESmokeTest(){
+		DataStorageSystem testDSImplementation = new DataStorageImplementation();
+		ComputeEngine testComputeEngine = new ComputeEngine();
+		InputConfig testFile = new FileInputConfig("TestFile");
+		UUID key = testDSImplementation.sendData(testFile);
+		ArrayList<Integer> testAl = testDSImplementation.recieveData(key);
+		ArrayList<char[]> sortedFileArr = testComputeEngine.readFile(testAl);
+		//checks to make sure that the amount of char[] is the same as the amount of integers in the file
+		Assertions.assertEquals(sortedFileArr.size(),testAl.size());
+	}
+}
