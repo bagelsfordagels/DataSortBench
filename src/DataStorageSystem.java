c02faa7ae3b6d1f662a9cb6f936dda477a6ecaf9
Suchya2 <suchya2@newpaltz.edu>
2024-10-05 13:08:44 -0400

    DataStoreExceptions

--- src/DataStorageSystem.java
+++ src/DataStorageSystem.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 public interface DataStorageSystem {
-	public UUID sendData(InputConfig userdata);
-	public ArrayList<Integer> recieveData(UUID key);
+	public UUID sendData(InputConfig userdata) throws Exception;
+	public ArrayList<Integer> recieveData(UUID key) throws Exception;
 	public File mkFile(ArrayList<char[]> charAl) throws IOException;
 }
 
